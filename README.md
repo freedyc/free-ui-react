f9d8e3c924e248e61a59b96db710b84fc03d4222
dengyongchao <dengyongchao@zdns.cn>
2018-06-07 12:42:03 +0800

    Usage

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# react-suit
+<p align="center">
+    <h3 align="center">Free UI React</h3>
+</p>
+
+## Content
+
+- Modal
+- Portal
+
+## Quick start
+
+- npm i @dengyongchao/modal --save
+```
+import React, { Component }  from 'react';
+import Modal from '@dengyongchao/modal';
+import '@dengyongchao/modal/modal.css';
+
+class Example extends Component {
+    state = {
+        open: false,
+        header: false,
+        mask: false
+    }
+
+    show = () => {
+        this.setState({ open: true, showHeader: true, showMask: true  })
+    }
+
+    render() {
+        return (
+            <div>
+                <button type="button" onClick={ this.show }>Show Modal </button>
+                <Modal {...this.state} close={this.close} size={{width: 500, height: 300}}>
+                    <h1>You Contnet</h1>
+                </Modal>
+            </div>
+        )
+    }
+};
+
+export default Example;
+
+```
--- packages/modal-demo/src/example.js
+++ packages/modal-demo/src/example.js
@@ -3,22 +3,34 @@ import Modal from '@dengyongchao/modal';
 import '@dengyongchao/modal/modal.css';
 
 class Example extends Component {
-    state = { open: false, header: false, mask: false }
+    state = {
+        open: false,
+        header: false,
+        mask: false
+    }
 
-    show = () => { this.setState({ open: true, showHeader: true, showMask: true  })}
-    show1 = () => { this.setState({ open: true, showHeader: true, showMask: false  })}
-    show2 = () => { this.setState({ open: true, showHeader: false, showMask: false })}
+    show = () => {
+        this.setState({ open: true, showHeader: true, showMask: true  })
+    }
 
-    close = () => { this.setState({ open: false })}
+    show1 = () => {
+        this.setState({ open: true, showHeader: true, showMask: false  })
+    }
 
-    ok = () => { this.close() }
+    show2 = () => {
+        this.setState({ open: true, showHeader: false, showMask: false })
+    }
+
+    close = () => {
+        this.setState({ open: false })
+    }
 
     render() {
         return (
             <div>
-                <button type="button" onClick={ this.show }>Show All</button>
-                <button type="button" onClick={ this.show1 }>Hide Mask</button>
-                <button type="button" onClick={ this.show2 }>Hide Header, Mask</button>
+                <button type="button" onClick={ this.show }>Show Modal </button>
+                <button type="button" onClick={ this.show1 }>Show Modal --Hide Mask</button>
+                <button type="button" onClick={ this.show2 }>Show Modal --Hide Header, Mask</button>
                 <Modal {...this.state} close={this.close} size={{width: 500, height: 300}}>
                     <h1>You Contnet</h1>
                     <h2>You content</h2>
@@ -31,7 +43,5 @@ class Example extends Component {
         )
     }
 };
-
-
 
 export default Example;
