360a1823ef0d076dc42a9931573965d537070ae4
dengyongchao <dengyongchao@zdns.cn>
2018-11-20 18:34:16 +0800

    [react-suit] demo fix

--- packages/portal-demo/src/App.js
+++ packages/portal-demo/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Portal from 'h-portal';
+import Portal from '@dengyongchao/portal';
 
 class App extends Component {
   render() {
